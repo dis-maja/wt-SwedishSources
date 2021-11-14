6c4941271a1a50cd8992440bcfc5d7e816116600
dis-maja <80508009+dis-maja@users.noreply.github.com>
2021-11-14 12:40:40 +0100

    Add files via upload
    Allow manual Repositories.

--- Http/SwedishSourcesBooksAction.php
+++ Http/SwedishSourcesBooksAction.php
@@ -75,8 +75,10 @@ class SwedishSourcesBooksAction implements RequestHandlerInterface
 		    $i = $k;
 		}
 	    }
-	    $repo_from[] = '@' . trim(str_replace('1 RIN ','', $tmp[$i])) . '@';
-	    $repo_to[] = '@' . trim($val->o_id) . '@';
+	    if ($i !== -1) {
+		$repo_from[] = '@' . trim(str_replace('1 RIN ','', $tmp[$i])) . '@';
+		$repo_to[] = '@' . trim($val->o_id) . '@';
+	    }
 	}
 
 	$url = "";
